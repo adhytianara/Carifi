58979f49792e99bc1bcdbd43256e28022683644d
Adhytia <adit.14.12.99@gmail.com>
2020-11-16 12:30:14 +0700

    Implementing bottom navigation

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/carifi/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/carifi/MainActivity.java
@@ -1,14 +1,56 @@
 package id.ac.ui.cs.mobileprogramming.adhytia.carifi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNav;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_movie:
+                            selectedFragment = new MovieFragment();
+                            break;
+                        case R.id.nav_tvShow:
+                            selectedFragment = new TvShowFragment();
+                            break;
+                        case R.id.nav_people:
+                            selectedFragment = new PeopleFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, selectedFragment)
+                            .commit();
+                    return true;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new MovieFragment())
+                .commit();
     }
 }
