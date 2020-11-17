3442ff1a453cd997b22e552580619b7410fcd85a
Adhytia <adit.14.12.99@gmail.com>
2020-11-17 23:42:25 +0700

    Implement parcelable

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/carifi/movie/model/Movie.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/adhytia/carifi/movie/model/Movie.java
@@ -1,7 +1,31 @@
 package id.ac.ui.cs.mobileprogramming.adhytia.carifi.movie.model;
 
-public class Movie {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
     private String title, detail, posterURL;
+
+    protected Movie(Parcel in) {
+        title = in.readString();
+        detail = in.readString();
+        posterURL = in.readString();
+    }
+
+    public Movie() {
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -25,5 +49,17 @@ public class Movie {
 
     public void setPosterURL(String posterURL) {
         this.posterURL = posterURL;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(detail);
+        dest.writeString(posterURL);
     }
 }
